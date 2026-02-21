b3d4fc39a34da93f5ef7284e6778fc1d6985ebe5
vivek <codecrafter67@gmail.com>
2026-02-21 20:20:42 +0530

    implement the hosted tools agent

---
+++ app/05_hosted__tool_agent.py
@@ -0,0 +1,37 @@
+from openai import AsyncOpenAI
+from agents import Agent, OpenAIChatCompletionsModel, Runner, WebSearchTool, FileSearchTool
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+gemini_api_key = os.getenv('GOOGLE_API_KEY')
+model = os.getenv("GEMINI_MODEL")
+
+client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+
+
+
+agent = Agent(
+    name="Assistant",
+    instructions="You are an expert of agentic AI.",
+    model=OpenAIChatCompletionsModel(model=model, openai_client=client),
+    tools = [
+            WebSearchTool(),
+           ]
+)
+
+
+query = input("Enter the query: ")
+
+result = Runner.run_sync(
+    agent,
+    query,
+)
+
+print(result.final_output)
