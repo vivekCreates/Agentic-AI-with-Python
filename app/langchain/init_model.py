3150d66d00522f429ffbc5286ed7c554ea0cc32d
vivek <codecrafter67@gmail.com>
2026-02-27 12:30:22 +0530

    Add initial model setup for GPT-4.1 with environment variable support

---
+++ app/langchain/init_model.py
@@ -0,0 +1,13 @@
+import os
+from langchain.chat_models import init_chat_model
+from dotenv import load_dotenv
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
+model = init_chat_model("gpt-4.1")
+
+query = input("Enter the prompt? ")
+response = model.invoke(query)
+
+print(response)
