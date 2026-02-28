fdf7c16abf5483864a0cbbd60be351cbc4fd753e
vivek <codecrafter67@gmail.com>
2026-02-28 16:31:18 +0530

    Refactor Movie model fields to use Pydantic's Field with descriptions and add nested MovieDetail model for enhanced movie data handling

--- app/langchain/nested_structured_output.py
+++ app/langchain/nested_structured_output.py
@@ -1,7 +1,10 @@
 import os
 from langchain_groq import ChatGroq
 from pydantic import BaseModel,Field
+from typing import List
 from dotenv import load_dotenv
+
+
 load_dotenv()
 
 os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
@@ -9,15 +12,24 @@ model_name = os.getenv("GROQ_MODEL")
 
 model = ChatGroq(model=model_name)
 
-class Movie(BaseModel):
+class Actor(BaseModel):
+    name:str=Field(...,description="Name of actor")
+    age:int=Field(...,description="Age of actor")
+
+
+class MovieDetail(BaseModel):
     title:str=Field(...,description="Title of the movie")
     year:str=Field(...,description="Year of the movie")
+    cast:List[Actor] = Field(...,description="Cast of the movie")
     director:str=Field(...,description="Director of the movie")
-    rating:float=Field(...,description="Ratings of the movie")
+    budget:float=Field(...,description="Budget of the movie")
+    profit:float=Field(...,description="Profit of the movie")
+    
+    
+model_with_structure = model.with_structured_output(MovieDetail)
 
-model_with_structure = model.with_structured_output(Movie)
+prompt = input("Enter movie name: ")
 
-prompt = input("Enter prompt: ")
 response = model_with_structure.invoke(prompt)
 
 print(response)
