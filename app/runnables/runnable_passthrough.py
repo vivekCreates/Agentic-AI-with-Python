b5e0201a83c82d9143ca752d53c76ce296f3a1d5
vivek <codecrafter67@gmail.com>
2026-04-09 13:19:19 +0530

    Add runnable implementation for ChatGroq with code generation and explanation

---
+++ app/runnables/runnable_passthrough.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+model_name = os.getenv("GROQ_MODEL")
+
+model = ChatGroq(model=model_name)
+parser = StrOutputParser()
+
+code_prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a code generator"),
+    ("human", "{topic}")
+])
+
+explain_prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful assistant who explains code in simple terms"),
+    ("human", "Explain the following code in simple words:\n{code}")
+])
+
+seq = code_prompt | model | parser 
+
+
+seq2 = RunnableParallel(
+    {"code" :  RunnablePassthrough(),
+     "explanation" : explain_prompt | model | parser
+    }
+)
+
+chain = seq | seq2
+
+result = chain.invoke({"topic" : "please write a code of palindrome in python "})
+
+print(result['code'])
+print(result['explanation'])
